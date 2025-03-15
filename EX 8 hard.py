295a81097999022f8bd7de5ddba51ef4349a05c4
Minotti16 <mingottallan16@gmail.com>
2025-03-15 13:42:36 -0300

    Aula 2

--- EX 8 hard.py
+++ EX 8 hard.py
@@ -1,7 +1,5 @@
 base = int(input('digite a base: '))
 expoente = int(input('digite um expoente: '))
 
-for i in range(1,expoente):
-    base ^= base
-    print(base, end= ' ')for i in range(1):
+for i in range(1):
     print(base ** expoente)
