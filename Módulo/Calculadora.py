5b0dbc110f3c78fe06cab16633b710d53b979ff7
Minotti16 <mingottallan16@gmail.com>
2025-03-29 17:20:27 -0300

    Aula 2

---
+++ Módulo/Calculadora.py
@@ -0,0 +1,24 @@
+def somar(a,b):
+   return a + b
+
+
+
+
+def subtrair(a,b):
+    return a - b
+
+
+
+
+def multiplicar(a,b):
+    return a * b
+
+
+
+
+
+
+def divisão(a,b):
+    return a / b
+
+
---
+++ Módulo/Módulo.py
@@ -0,0 +1,17 @@
+import os
+import menu
+import Calculadora
+
+menu.menu()
+
+
+
+
+
+
+
+
+
+
+
+
