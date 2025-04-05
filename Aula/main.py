1e730882c0883e0a6f861f1f49c823c73b8c8cf6
Minotti16 <mingottallan16@gmail.com>
2025-04-05 16:41:07 -0300

    Aula 10

--- Aula/main.py
+++ Aula/main.py
@@ -1,1 +1,8 @@
-277353
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+
+import os
+
+def gerar_criptografia():
+    os.urandom(32)
