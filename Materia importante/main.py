5f5b80fc7f6e6328c6b752d68b55cd76b06e9f7d
Minotti16 <mingottallan16@gmail.com>
2025-04-26 17:29:38 -0300

    Aula 12

--- Materia importante/main.py
+++ Materia importante/main.py
@@ -1,4 +1,33 @@
 class Casa():
-    rua = "blue street"
-    bairro = "Good neighborhood"
-    CEP = "123654"
+
+    imobiliaria = 'Ctrl Imóveis'
+
+    def __init__(self, rua, bairro, CEP):
+        self.rua = rua
+        self.bairro = bairro
+        self.CEP = CEP
+
+    def getImobiliaria(self):
+        return self.imobiliaria
+    
+    def getRua(self):
+        return self.rua
+    
+    def getBairro(self):
+        return self.bairro
+    
+    def getCEP(self):
+        return self.CEP
+    
+    def setImobiliaria(self,i):
+        self.imobiliaria = i
+
+    def setRua(self,r):
+        self.rua = r
+    
+    def setBairro(self,b):
+        self.bairro = b
+
+    def setCEP(self, c):
+        self.CEP = c
+
