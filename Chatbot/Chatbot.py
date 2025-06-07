87baccf45ede38750f191cac898fbcee7ce97d81
Minotti16 <mingottallan16@gmail.com>
2025-06-07 17:15:50 -0300

    Aula 18

---
+++ Chatbot/Chatbot.py
@@ -0,0 +1,9 @@
+import main2 as pc
+
+nome_maquina = 'Maria'
+pc.saudacoes(nome_maquina)
+while True:
+    texto = pc.receberTexto()
+    resposta = pc.buscarResposta(nome_maquina, texto)
+    if pc.exibeResposta(resposta, nome_maquina) == 'fim':
+        break
--- Chatbot/main2.py
+++ Chatbot/main2.py
@@ -1,40 +1,39 @@
 def saudacoes(nome):
     import random
-    frases = ['Bom dia! Meu nome é ' + nome + '. Como vai você?', 'Olá!', 'oi, tudo']
+    frases = ['Bom dia! Meu nome é ' + nome + '. Como vai você?', 'Olá!', 'oi, tudo?']
     print(frases[random.randint(0,2)])
 
 def receberTexto():
-    texto = 'Cliente:'+ input('Cliente:')
-    palavraProibido = ['caralho']
+    texto = 'Cliente: ' + input('Cliente: ')
+    palavraProibido = ['bocó']
     for p in palavraProibido:
         if p in texto:
-            print('Não vem não! Me respeite!')
+            print('Não vem não! Me respeite')
             return(receberTexto())
         return texto
     
 def buscarResposta(nome, texto):
-    with open ('chat.txt', 'a+') as conhecimento:
+    with open('chat.txt','a+') as conhecimento:
         conhecimento.seek(0)
         while True:
             viu = conhecimento.readline()
-            if viu != "":
-                if texto.replace('Cliente:', '') == 'Tchau':
-                    print(nome+ ':volte sempre!')
+            if viu != '':
+                if texto.replace('Cliente: ','') == 'Tchau':
+                    print(nome + ': volte sempre!')
                     return 'fim'
                 elif viu.strip() == texto.strip():
                     proximalinha = conhecimento.readline()
                     if 'Chatbot: ' in proximalinha:
                         return proximalinha
             else:
-                print('Me desculpe, não sei o que falar')
-                conhecimento.write('\n' +texto)
+                print('Me desculpe não sei o que falar')
+                conhecimento.write('\n' + texto)
                 resposta_user = input('O que esperava?\n')
-                conhecimento.write('\n' +'Chatbot:' +resposta_user)
-                return 'Hum...'
-            
+                conhecimento.write('\n' + 'Chatbot: ' + resposta_user)
+                return 'Hum....'
+
 def exibeResposta(resposta, nome):
     print(resposta.replace('Chatbot', nome))
     if resposta == 'fim':
         return 'fim'
     return 'continua'
-
